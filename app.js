f6a57a3c1bfe1b80880030359028d3481cb13c09
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2020-11-25 18:27:29 +0530

    Added code for server which reads input from user and stores in file from Udemy Course

--- app.js
+++ app.js
@@ -1,53 +1,6 @@
 const http = require('http');
-
-const server = http.createServer((req, res)=> {
-    console.log(req.url, req.method, req.headers);
-    
-    const url = req.url;
-    const method = req.method;
-    const fs = require('fs');
-
-    if(url === '/')
-    {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form action = "/message" method = "POST"><input type="text" name = "message"><button type="submit">Send</button></form></body>');
-    // we are sending  'post' request to "/message" page    
-    res.write('</html>');
-    return res.end(); // this marks end of response, nodejs will send the written response to the client
-    }
-    if(url === '/message' && method === 'POST')
-    {
-        const body = [];
-        req.on('data', (chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-
-        req.on('end', ()=>
-        {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            
-            console.log(parsedBody);
-            fs.writeFileSync('Respone.txt', message);
-        })
-        
-        // fs.writeFileSync('Respone.txt', 'Dummy response');
-        res.statusCode = 302; // this implies to browser that redirecting will occur
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page    </title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-
-    res.write('</html>');
-    res.end();
+const routes = require('./routes');
 
 
-
-});
-
+const server = http.createServer(routes);
 server.listen(3000);
